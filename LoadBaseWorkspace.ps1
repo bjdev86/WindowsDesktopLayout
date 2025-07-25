aa1f27f28838d69264c3dd07de495ae6d907a902
Ben Miller <milleb2@XL0443BM.odot.state.or.us>
2025-07-25 09:20:50 -0700

    Added 'flower box' at head of primary script.

--- LoadBaseWorkspace.ps1
+++ LoadBaseWorkspace.ps1
@@ -1,3 +1,21 @@
+<#
+.AUTHOR
+    Ben Miller
+.DATE
+    7/24/2025
+
+.SYNOPSIS
+    Load the base workspace.
+.DESCRIPTION
+    Load the base workspace. Open Outlook in the center half and Edge on the left quarter. 
+.EXAMPLE
+    LoadBaseWorkspace.ps1
+
+TODO: 
+    - Add a parameter to specify the workspace to load. 
+    - Provide for the passing of arguments to applicaitons loaded when the workspace is loaded.
+#>
+
 # Import modules
 . "$PSScriptRoot\ThreeWindow-Centered.ps1"
 
@@ -5,4 +23,3 @@
 # Open Outlook in the center half and Edge on the left quarter. 
 Start-Center "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" "Outlook"; 
 Start-Left  "msedge.exe";
-Start-Right "notepad.exe"; 
