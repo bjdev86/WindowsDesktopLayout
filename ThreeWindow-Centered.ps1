bb889a11b317f5ecae17aa589ddc7da64aacec12
Ben Miller <milleb2@XL0443BM.odot.state.or.us>
2025-07-24 19:09:29 -0700

    Moved logic out to module that will house and run logic for ThreeWindowed configuration with center window being the largest.

---
+++ ThreeWindow-Centered.ps1
@@ -0,0 +1,96 @@
+# Module to build a three window centered workspace. Where the middle window is largest.
+
+# Add necessary types for window manipulation (Import user32.dll)
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class WindowManipulation
+{
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+}
+"@
+
+# Constants
+New-Variable -Name SCRNWIDTH -Value ([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width) -Option Constant
+New-Variable -Name SCRNHEIGHT -Value ([System.Windows.Forms.SystemInformation]::WorkingArea.Height) -Option Constant
+
+# Function to start a process and move its window
+function Start-And-Position 
+{
+    param (
+        [string]$processPath,
+        [int]$x,
+        [int]$y,
+        [int]$width,
+        [int]$height
+    )
+
+    # Start the application process
+    $process = Start-Process -FilePath $processPath -PassThru;
+    
+    # Wait for the process to start and the window to appear
+    Start-Sleep -Seconds 1; 
+
+    # Parse the process name (The pure name of the process sans path and extension)
+    $proName = @($processPath -split "[\\\.]")[-2];   
+
+    # Find the window handle
+    $hWnd = Get-Process -Name $proName | Select-Object -ExpandProperty MainWindowHandle;
+    
+    # For windows/processes with the same name filter off the first hanldle. It's the one tied to the process.
+    $hWnd = $hWnd -is [System.Array] ? $hWnd[0] : $hWnd;  
+    if ($hWnd -is [System.Array]) { Write-Host "Is Array: $hWnd" } else { Write-Host "Is Not Array: $hWnd" } 
+    
+    if ($hWnd -ne [IntPtr]::Zero) 
+    {
+        # Move the window
+        [WindowManipulation]::MoveWindow($hWnd, $x, $y, $width, $height, $true)
+    } 
+    else 
+    {
+        #TODO: Wrap in try/catch and attempt to retry.
+        Write-Host "Window not found: $procName"
+    }
+}
+
+function Start-Center ([string]$processPath) 
+{
+    # Calculate the window size
+    [int]$windowWidth = $SCRNWIDTH / 2; 
+    [int]$windowHeight = $SCRNHEIGHT;
+
+    # Calculate the window position
+    $x = ($SCRNWIDTH / 4);
+    $y = 0;
+
+    Start-And-Position $processPath $x $y $windowWidth $windowHeight 
+}
+
+function Start-Left ([string]$processPath)
+{  
+    # Calculate the window size
+    [int]$windowWidth = $SCRNWIDTH / 4; 
+    [int]$windowHeight = $SCRNHEIGHT;
+
+    # Calculate the window position
+    $x = 0;
+    $y = 0;
+
+    Start-And-Position $processPath $x $y $windowWidth $windowHeight 
+}
+
+function Start-Right ([string]$processPath)
+{  
+    # Calculate the window size
+    [int]$windowWidth = $SCRNWIDTH / 4; 
+    [int]$windowHeight = $SCRNHEIGHT;
+
+    # Calculate the window position
+    $x = ($SCRNWIDTH - $windowWidth);
+    $y = 0;
+
+    Start-And-Position $processPath $x $y $windowWidth $windowHeight 
+}
